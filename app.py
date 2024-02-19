069d34cf60b7bcb49c8ac2b003b591748803cf99
tzhu27 <tzhu27@illinois.edu>
2024-02-19 17:32:58 -0600

    Dropdown now lists all the companies

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def read_csv() -> dict:
+    df = pd.read_csv('twcs.csv')
+    return df.to_dict(orient='records')
+
+def get_companies(df_dict: list) -> dict:
+    return {tweet['author_id'] for tweet in df_dict if 'author_id' in tweet and not(tweet['author_id'].isnumeric())}
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    tweets_lst = read_csv()
+    companies = get_companies(tweets_lst)
+
+    return render_template('index.html', companies=companies)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
